62f27192e2e2f0b6d7f01eca088abdbfaed75146
maina lawrence <mainalawrence32@gmail.com>
2022-05-24 14:30:53 +0300

    updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { UsersComponent } from './users/users.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
     LoginComponent,
-    NavBarComponent
+    NavBarComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
